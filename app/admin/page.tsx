4687b2a25e79f56a4127585cd1adbb2b93fe6ed1
Biscute-kk <arunsubedi170@gmail.com>
2026-03-25 14:15:15 +0545

    initial push

---
+++ app/admin/page.tsx
@@ -0,0 +1,18 @@
+export default function Page(){
+    return (
+        <>
+            <div>
+                <p>Dashboard</p>
+            </div>
+            <div>
+                <p>My Blogs</p>
+            </div>
+
+            <div>
+                <p>add new blog</p>
+            </div>
+
+
+        </>
+    )
+}   
---
+++ app/login/page.tsx
@@ -0,0 +1,23 @@
+export default function Page(){
+    return(
+        <>
+            <div >
+                <p>Login Page</p>
+            </div>
+            <div>
+                <p>Username</p>
+            </div>
+            <input/>
+            <div>
+                <p>Password</p>
+            </div>
+            <input/>
+            <div>
+                <button className="border">Submit</button>
+            </div>
+            <div>
+                <p>Create an account.</p>
+            </div>
+        </>
+    );
+}
